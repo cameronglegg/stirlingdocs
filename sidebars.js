108277b3e7ca4c43c498fe0a731c5aff426f6f11
Cameron <cameron@stirlingwoodworks.com>
2021-06-04 15:53:06 -0700

    Moved around sidebar stuff

--- sidebars.js
+++ sidebars.js
@@ -3,21 +3,22 @@ module.exports = {
     Stirling: ['doc1',
     {
       Communication: ['doc3', 'externalcomm', 'terminology']
-    },
-    {
-      Planning: ['doc2', 'measureguide', 'digimark']
-    },
-    {
-      Production: ['mixformula', 'modernshaker']
-    },
-    {
-      Delivery: ['delivery', 'delivery2']
-    },
-    {
-      Install: ['installation', 'installation2']
-    },
-    
-  'toollist'],
+    }, 
+    'toollist'],
+    Standard Procedures: [
+      {
+        Planning: ['doc2', 'measureguide', 'digimark']
+      },
+      {
+        Production: ['mixformula', 'modernshaker']
+      },
+      {
+        Delivery: ['delivery', 'delivery2']
+      },
+      {
+        Install: ['installation', 'installation2']
+      },
+    ],
     Roles: ['roles', 'cncop', 'leadhand', 'purchasing'],
     Features: ['mdx', 'cheatsheet'],
   },
