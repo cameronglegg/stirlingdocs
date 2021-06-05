63e96d019197762f9d8ea3bc24ac6ad8225f207f
Cameron <cameron@stirlingwoodworks.com>
2021-06-05 16:15:34 -0700

    Significant changes to sidebar. Could stil be broken.

--- sidebars.js
+++ sidebars.js
@@ -1,26 +1,185 @@
 module.exports = {
-  someSidebar: {
-    Stirling: ['doc1',
+  docs: [
+    'doc1',
     {
-      Communication: ['doc3', 'externalcomm', 'terminology']
+      type: 'category',
+      label: 'Communication',
+      collapsed: false,
+      items: ['doc3', 'externalcomm', 'terminology'],
     },
-
-'toollist'],
-    SOPs: ['sopintroduction',
-      {
-        Planning: ['doc2', 'measureguide', 'digimark']
-      },
-      {
-        Production: ['mixformula', 'modernshaker']
-      },
-      {
-        Delivery: ['delivery', 'delivery2']
-      },
-      {
-        Install: ['installation', 'installation2']
-      },
+    {
+      type: 'category',
+      label: 'Features',
+      collapsed: false,
+      items: ['mdx', 'cheatsheet'],
+    },
+    'toollist'],
+  sops: [
+    'sopintroduction',
+    {
+      type: 'category',
+      label: 'Planning',
+      collapsed: false,
+      items: ['doc2', 'measureguide', 'digimark']
+    },
+    {
+      type: 'category',
+      label: 'Production',
+      collapsed: false,
+      items: ['mixformula', 'modernshaker']
+    },
+    {
+      type: 'category',
+      label: 'Delivery',
+      collapsed: false,
+      items: ['delivery', 'delivery2']
+    },
+    {
+      type: 'category',
+      label: 'Install',
+      collapsed: false,
+      items: ['installation', 'installation2']
+    },
     ],
-    Roles: ['roles', 'cncop', 'leadhand', 'purchasing'],
-    Features: ['mdx', 'cheatsheet'],
-  },
+  roles: [
+     'roles',
+    {
+      type: 'category',
+      label: 'Roles',
+      collapsed: false,
+      items: ['cncop', 'leadhand', 'purchasing']
+    },
+  ]
 };
+
+// This is from Docusaurus and is a much better version of a sidebar. We will copy this moving forward.
+/*
+
+module.exports = {
+ docs: [
+   'introduction',
+   {
+     type: 'category',
+     label: 'Getting Started',
+     collapsed: false,
+     items: ['installation', 'configuration', 'typescript-support'],
+   },
+   {
+     type: 'category',
+     label: 'Guides',
+     items: [
+       'guides/creating-pages',
+       {
+         Docs: [
+           'guides/docs/introduction',
+           'guides/docs/create-doc',
+           'guides/docs/sidebar',
+           'guides/docs/versioning',
+           'guides/docs/markdown-features',
+           'guides/docs/multi-instance',
+         ],
+       },
+       'blog',
+       {
+         type: 'category',
+         label: 'Markdown Features',
+         items: [
+           'guides/markdown-features/introduction',
+           'guides/markdown-features/react',
+           'guides/markdown-features/tabs',
+           'guides/markdown-features/code-blocks',
+           'guides/markdown-features/admonitions',
+           'guides/markdown-features/headings',
+           'guides/markdown-features/inline-toc',
+           'guides/markdown-features/assets',
+           'guides/markdown-features/plugins',
+           'guides/markdown-features/math-equations',
+         ],
+       },
+       'styling-layout',
+       'static-assets',
+       'search',
+       'browser-support',
+       'deployment',
+       {
+         type: 'category',
+         label: 'Internationalization',
+         items: [
+           {
+             type: 'doc',
+             id: 'i18n/introduction',
+             label: 'Introduction',
+           },
+           {
+             type: 'doc',
+             id: 'i18n/tutorial',
+             label: 'Tutorial',
+           },
+           {
+             type: 'doc',
+             id: 'i18n/git',
+             label: 'Using Git',
+           },
+           {
+             type: 'doc',
+             id: 'i18n/crowdin',
+             label: 'Using Crowdin',
+           },
+         ],
+       },
+     ],
+   },
+   {
+     type: 'category',
+     label: 'Advanced Guides',
+     items: ['using-plugins', 'using-themes', 'presets'],
+   },
+   {
+     type: 'category',
+     label: 'Migrating from v1 to v2',
+     items: [
+       'migration/migration-overview',
+       'migration/migration-automated',
+       'migration/migration-manual',
+       'migration/migration-versioned-sites',
+       'migration/migration-translated-sites',
+     ],
+   },
+ ],
+ api: [
+   'cli',
+   'docusaurus-core',
+   'api/docusaurus.config.js',
+   'lifecycle-apis',
+   {
+     type: 'category',
+     label: 'Plugins',
+     items: [
+       'api/plugins/plugins-overview',
+       'api/plugins/plugin-content-docs',
+       'api/plugins/plugin-content-blog',
+       'api/plugins/plugin-content-pages',
+       'api/plugins/plugin-client-redirects',
+       'api/plugins/plugin-debug',
+       'api/plugins/plugin-google-analytics',
+       'api/plugins/plugin-google-gtag',
+       'api/plugins/plugin-ideal-image',
+       'api/plugins/plugin-pwa',
+       'api/plugins/plugin-sitemap',
+     ],
+   },
+   {
+     type: 'category',
+     label: 'Themes',
+     items: [
+       'api/themes/themes-overview',
+       'api/themes/theme-configuration',
+       'api/themes/theme-classic',
+       'api/themes/theme-bootstrap',
+       'api/themes/theme-live-codeblock',
+       'api/themes/theme-search-algolia',
+     ],
+   },
+ ],
+};
+*/
