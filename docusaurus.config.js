bf32e48a297ae4d0614f75d583d9ba0c80e488ac
Cameron <cameron@stirlingwoodworks.com>
2021-06-05 21:35:40 -0700

    Found a snippet from Docusaurus that adds tha Nav bar. Not sure if this is going to help...

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -131,3 +131,68 @@ module.exports = {
     ],
   ],
 };
+
+/* This is commented out and related to adding the Navbar in.
+    navbar: {
+      hideOnScroll: true,
+      title: 'Docusaurus',
+      logo: {
+        alt: 'Docusaurus Logo',
+        src: 'img/docusaurus.svg',
+        srcDark: 'img/docusaurus_keytar.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'introduction',
+          label: 'Docs',
+        },
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'cli',
+          label: 'API',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'showcase', label: 'Showcase', position: 'left'},
+        {
+          to: '/community/support',
+          label: 'Community',
+          position: 'left',
+          activeBaseRegex: `/community/`,
+        },
+        // right
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
+          dropdownItemsAfter: [
+            {
+              to: 'https://v1.docusaurus.io',
+              label: '1.x.x',
+            },
+            {
+              to: '/versions',
+              label: 'All versions',
+            },
+          ],
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+          dropdownItemsAfter: [
+            {
+              to: 'https://github.com/facebook/docusaurus/issues/3526',
+              label: 'Help Us Translate',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+*/
