6986c65429bd857af35ecdf049b1744827248602
cameronglegg <cameron@stirlingwoodworks.com>
2021-06-24 15:47:00 -0700

    added doc for scraper

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
       type: 'category',
       label: 'Communication',
       collapsed: false,
-      items: ['howtosubmitdoc', 'howtodocs','doc3', 'externalcomm', 'terminology'],
+      items: ['howtosubmitdoc', 'howtodocs', 'howtorunscraper','doc3', 'externalcomm', 'terminology'],
     },
     {
       type: 'category',
